c4e75f3b7c77251f15986e7a6ca09ff0b5a77f72
Eugene Krylov <eugene@eugenekrylov.com>
2019-11-29 18:42:26 -0500

    Prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "printWidth": 80,
-  "tabWidth": 2,
-  "useTabs": false,
-  "singleQuote": true,
-  "trailingComma": "all",
-  "bracketSpacing": false,
-  "jsxBracketSameLine": false,
-  "proseWrap": "always"
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  proseWrap: 'always',
 };
